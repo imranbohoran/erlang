fd6bffa4e1469be9cd670a6b66c27d8a1c2ca5c0
Imran Bohoran <imranbohoran@gmail.com>
2017-03-07 01:26:21 +0000

    Only using pattern matching

--- part1.erl
+++ part1.erl
@@ -6,4 +6,4 @@ fib_tail_recursive(1, FibValue, _NextValue) -> FibValue;
 
 % Tail recursive function when iteration is more than 1. When it's 1, we return the FibValue from the above definision
 % For 6 -> (6,1,1) -> (5,1,2) -> (4,2,3) -> (3,3,5) -> (2, 5, 8) -> (1, 8, 13) -> hits the first definition
-fib_tail_recursive(Iteration, FibValue, NextValue) when Iteration > 1 -> fib_tail_recursive(Iteration - 1, NextValue, FibValue + NextValue).
+fib_tail_recursive(Iteration, FibValue, NextValue) -> fib_tail_recursive(Iteration - 1, NextValue, FibValue + NextValue).
