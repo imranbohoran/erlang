0bdaa37c8c4694fb3059c9665a85d265a24359cf
Imran Bohoran <imranbohoran@gmail.com>
2017-03-07 21:31:41 +0000

    Handling fib(0) instances.

--- part1.erl
+++ part1.erl
@@ -2,6 +2,7 @@
 - export([fib/1]).
 
 fib(N) -> fib_tail_recursive(N, 1, 1).
+fib_tail_recursive(0, _FibValue, _NextValue) -> 1;
 fib_tail_recursive(1, FibValue, _NextValue) -> FibValue;
 
 % Tail recursive function when iteration is more than 1. When it's 1, we return the FibValue from the above definision
