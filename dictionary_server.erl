654fbf81741501a4c89951490ada0d1d388e8d1d
Imran Bohoran <imranbohoran@gmail.com>
2017-03-13 00:54:42 +0000

    Implementing insert and size functions.
     * A dictionary data structure is used to hold the key values for the dictionary server
     * The dictionary is passed around as a message within the dictionary_listener process.
     * Using the store function of the dictionary to insert values
     * The size function of the dictionary is used to get the size of the dictionary.

--- dictionary_server.erl
+++ dictionary_server.erl
@@ -7,10 +7,11 @@
 - export([clear/0]).
 - export([size/0]).
 
-start() -> 
-  Pid = spawn(fun() -> dictionary_listener() end),
+start() ->
+  Dictionary = dict:new(),
+  Pid = spawn(fun() -> dictionary_listener(Dictionary) end),
   register(dictionary_listener, Pid),
-  dictionary_listener ! {self(), command},
+  dictionary_listener ! {self(), start},
   receive
     _ -> "Dictonary Server - Started"
   end.
@@ -28,28 +29,30 @@ end_server(Pid) ->
   io:format("Dictionary Server - Stopped - ").
 
 
-dictionary_listener() ->
+dictionary_listener(Dictionary) ->
   receive
-    {From, Msg} ->
-      From ! {self(), Msg},
-      dictionary_listener();
+    {From, start} ->
+      io:format("Starting..."),
+      From ! {self(), "Started"},
+      dictionary_listener(Dictionary);
     {From, insert, Key, Value} ->
       io:format("Inserting..."),
       io:format(Key),
+      NewDictionary = dict:store(Key, Value, Dictionary),
       From ! {self(), "Inserting done"},
-      dictionary_listener();
+      dictionary_listener(NewDictionary);
     {From, remove, Key} ->
       io:format("Removing"),
-      dictionary_listener();
+      dictionary_listener(Dictionary);
     {From, lookup, Key} ->
       io:format("Lookup"),
-      dictionary_listener();
+      dictionary_listener(Dictionary);
     {From, clear} ->
       io:format("Clearing"),
-      dictionary_listener();
+      dictionary_listener(Dictionary);
     {From, size} ->
-      io:format("Inquiring size"),
-      dictionary_listener();
+      From ! {self(), dict:size(Dictionary)},
+      dictionary_listener(Dictionary);
     {From, stop} ->
       io:format("Stoppping...."),
       true
@@ -72,8 +75,19 @@ lookup(Key) ->
 
 
 clear() ->
-  erlang:error(not_implemented).
+  Pid = whereis(dictionary_listener),
+  Pid ! {self(), clear},
+  receive
+    _ -> ok
+  end.
 
 
 size() ->
-  erlang:error(not_implemented).
+  Pid = whereis(dictionary_listener),
+  Pid ! {self(), size},
+  io:format("The dictionary size is - "),
+  receive
+    {_Pid, Size} ->
+      Size;
+    _ -> ok
+  end.
