de4af12490a5e659b20770a8070b90d81b96343b
Imran Bohoran <imranbohoran@gmail.com>
2017-03-16 00:35:59 +0000

    Implementation the stop function and listening to any message
     * stop function will end the loop and exit the process.
     * any message received other than 'stop' would print out each element of the integer list.

--- integer_list.erl
+++ integer_list.erl
@@ -2,18 +2,29 @@
 - export([start/1]).
 - export([stop/1]).
 
-start(number_list) ->
-  spawn(fun () -> run(number_list) end).
+start(NumberList) ->
+  io:format("Starting..."),
+  Pid = spawn(fun () -> run(NumberList) end),
+  Pid.
 
+stop(Pid) ->
+  Pid ! {stop, self()},
+  receive
+    ok ->  exit(Pid, ok)
+  end.
 
-stop(pid) ->
-  erlang:error(not_implemented).
+print_list([]) ->
+  ok;
+print_list([H|T]) ->
+  io:format("~p",[H]),
+  print_list(T).
 
-run(numbers) ->
+run(Numbers) ->
   receive
     {stop, ReplyPid} ->
+      io:format("Stopping..."),
       ReplyPid ! ok;
-    {From, _} ->
-      From ! {self(), numbers},
-      run(numbers)
+    _ ->
+      print_list([Numbers]),
+      run(Numbers)
   end.
