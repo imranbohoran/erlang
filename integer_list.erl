f0cadbbc473a184cc231ec95b611d9b9a11b8f3f
Imran Bohoran <imranbohoran@gmail.com>
2017-03-20 01:48:07 +0000

    Adding capability to recive a filter message.
     * A filter message can be passed with a function that is then
       used for filtering the list.
     * Added tests for start and stop.

--- integer_list.erl
+++ integer_list.erl
@@ -1,4 +1,6 @@
 - module(integer_list).
+- include_lib("includes/eunit.hrl").
+
 - export([start/1]).
 - export([stop/1]).
 
@@ -24,7 +26,22 @@ run(Numbers) ->
     {stop, ReplyPid} ->
       io:format("Stopping..."),
       ReplyPid ! ok;
+    {filter, FilterFunction, ReplyPid} ->
+      io:format("Inside filter"),
+      Filtered = lists:filter(FilterFunction, Numbers),
+      io:format("Filtering done"),
+      print_list([Filtered]),
+      io:format("~n"),
+      ReplyPid ! [Filtered],
+      run(Numbers);
     _ ->
       print_list([Numbers]),
       run(Numbers)
   end.
+
+% Tests
+start_stop_test_() ->
+  TPid = start([1,3]),
+  ?_assert(is_process_alive(TPid)),
+  stop(TPid),
+  ?_assertNot(is_process_alive(TPid)).
